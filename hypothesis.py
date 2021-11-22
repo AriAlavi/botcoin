6a28a24224984d6ef58ecac8c842b2fa0937b7a0
AriAlavi <arianalavi1@gmail.com>
2021-11-21 22:04:12 -0800

    working on bolinger

--- hypothesis.py
+++ hypothesis.py
@@ -24,3 +24,30 @@ def bounce(shortTerm, longTerm, cash, botcoins, customParameters):
     else:
         customParameters["sell"] = True
         return Decimal(1)
+
+def hold(*args):
+    return Decimal(1)
+
+def bollingerBands(shortTerm, longTerm, cash, botcoins, customParameters):
+    assert isinstance(shortTerm, DiscreteData)
+    assert isinstance(longTerm, DiscreteData)
+    assert isinstance(cash, Decimal)
+    assert isinstance(botcoins, Decimal), "{} instead".format(type(botcoins))
+    assert isinstance(customParameters, dict)
+
+    BOLLINGER_BAND_TIME_PERIOD = 20
+
+    if not isinstance(customParameters.get("history", None), list):
+        customParameters["history"] = []
+    
+    if len(customParameters["history"]) == 0:
+        customParameters["history"].append(longTerm)
+    else:
+        if longTerm.date != customParameters["history"][-1].date:
+            customParameters["history"].append(longTerm)
+        if len(customParameters["history"]) > BOLLINGER_BAND_TIME_PERIOD:
+            customParameters["history"].pop(0)
+
+    # print(len(customParameters["history"]))
+
+    return Decimal(1)
